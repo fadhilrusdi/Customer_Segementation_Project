c9effd20b159d2cfd177b92bfcb0df2df71de986
Fadhil Rusdi <64071203+fadhilrusdi@users.noreply.github.com>
2021-04-15 11:17:48 +0700

    Update Customer segmentation 1.R

--- Customer segmentation 1.R
+++ Customer segmentation 1.R
@@ -47,4 +47,44 @@ ggplot(ssdata, aes(x=cluster,y=sse)) +
   geom_text(aes(label=format(round(sse, 2), nsmall = 2)),hjust=-0.2, vjust=-0.5) +
   scale_x_discrete(limits=c(1:jumlah_cluster_max))
 
+#menamai cluster
+#Lengkapi dengan dua vector bernama cluster dan Nama.Segmen
+
+Segmen.Pelanggan <- data.frame(cluster=c(1,2,3,4,5), 
+                               Nama.Segmen=c("Silver Youth Gals",
+                               "Diamond Senior Member", "Gold Young Professional", 
+                               "Diamond Professional", "Silver Mid Professional"))
+
+#Menggabungkan seluruh aset ke dalam variable Identitas.Cluster
+Identitas.Cluster <- list(Profesi=Profesi, Jenis.Kelamin=Jenis.Kelamin, 
+                          Tipe.Residen=Tipe.Residen, Segmentasi=segmentasi, 
+                          Segmen.Pelanggan=Segmen.Pelanggan, 
+                          field_yang_digunakan=field_yang_digunakan)
+
+saveRDS(Identitas.Cluster,"cluster.rds")
+
+#data baru
+databaru <- data.frame(Customer_ID="CUST-100", 
+                       Nama.Pelanggan="Rudi Wilamar",
+                       Umur=20,Jenis.Kelamin="Wanita",
+                       Profesi="Pelajar",
+                       Tipe.Residen="Cluster",
+                       NilaiBelanjaSetahun=3.5)
+
+Identitas.Cluster <- readRDS(file="cluster.rds")
+
+databaru <- merge(databaru, Identitas.Cluster$Profesi)
+databaru <- merge(databaru, Identitas.Cluster$Jenis.Kelamin)
+databaru <- merge(databaru, Identitas.Cluster$Tipe.Residen)
+
+#menentukan data baru di cluster mana
+
+which.min(sapply( 1:5, function( x ) sum( ( 
+  databaru[Identitas.Cluster$field_yang_digunakan] - 
+    Identitas.Cluster$Segmentasi$centers[x,])^2 ) ))
+Identitas.Cluster$Segmen.Pelanggan[which.min(sapply( 1:5, function( x ) 
+  sum( ( databaru
+         [Identitas.Cluster$field_yang_digunakan] - 
+           Identitas.Cluster$Segmentasi$centers[x,])^2 
+  ) )),]
 
